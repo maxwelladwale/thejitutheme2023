d098235140d9e65c0dba80916d23b04530d87163
Maxwell Adwale <48594933+maxwelladwale@users.noreply.github.com>
2023-05-03 17:11:06 +0300

    trancate text on banner

--- template-parts/home.php
+++ template-parts/home.php
@@ -13,13 +13,13 @@ get_header();
 ?>
         <!-- BANNER -->
         <section style="box-shadow: 0px 6px 9px 5px rgba(0, 0, 0, 0.1);">
-            <div class="container-fluid banner d-flex align-items-center" style="height:80vh">
+            <div class="container-fluid banner d-flex align-items-center" style="height:100vh">
                 <div class="container">
                     <div class="row align-items-start">
                         <div class="col-sm-12 col-lg-6 col-md-6">
-                            <h1>Lorem ipsum dolor sit amet <span class="redtext">consectetur</span> adipisicing.</h1>
+                            <h1>Augment your team <span class="redtext">accelerate </span> your success!</h1>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ratione eum laborum? Facere temporibus voluptas nemo distinctio inventore omnis repellendus ratione quisquam numquam commodi, cum veritatis quibusdam pariatur. Maiores, aperiam?
+								At THE Jitu, we offer comprehensive suite of services that helps your organization reduce costs, increase productivity, and gain access to top-notch talent. We take pride in offering customized and personalized solutions that cater to the unique needs of each company we work with.
                             </p>
                             <button class="button">Contact Us Today</button>
                         </div>
@@ -36,8 +36,8 @@ get_header();
             <div class="container">
                 <div class="row mt-5 mb-5">
                     <div class="col-md-6 col-sm-12">
-                        <h3 class="redtext">Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
-                        <p class="mt-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt aliquam iure quasi perferendis, esse cumque sequi qui necessitatibus nam dolorem aspernatur fugiat! Nam nihil nostrum explicabo ipsa suscipit nisi.</p>
+                        <h3 class="redtext">Our Services</h3>
+                        <p class="mt-3"> THE Jitu offers extra talent needed in Front Office and Back Office services and Technology Solutions. In addition, The Jitu offers a training program to empower young software developers in Kenya.</p>
                     </div>
     
                     <div class="col-md-6 col-sm-12">
@@ -53,7 +53,7 @@ get_header();
                                 <div class="col-md-10">
                                     <div class="card-body">
                                         <h5 class="card-title">Technology</h5>
-                                        <p class="card-text">Card text goes here.</p>
+                                        <p class="card-text">Our IT Services Solutions are designed to help you streamline your operations, reduce costs, and increase efficiency. </p>
                                     </div>
                                 </div>
                             </div>
@@ -72,7 +72,7 @@ get_header();
 									<div class="col-md-10">
 										<div class="card-body">
 											<h5 class="card-title">BPO</h5>
-											<p class="card-text">Card text goes here.</p>
+											<p class="card-text">THE Jitu BPO services are designed to improve customer service and increase efficiency in customer facing operations. </p>
 										</div>
 									</div>
 								</div>
@@ -91,7 +91,7 @@ get_header();
 									<div class="col-md-10">
 										<div class="card-body">
 											<h5 class="card-title">TRAINING</h5>
-											<p class="card-text">Card text goes here.</p>
+											<p class="card-text">THE Jitu offers a unique opportunity for young software developers in Kenya to gain invaluable experience by working in a fully-fledged software company. </p>
 										</div>
 									</div>
 								</div>
@@ -112,40 +112,43 @@ get_header();
                 <div class="container">
                     <div class="row py-5 text-center">
                         <div class="col-12 d-flex flex-column align-items-center">
-                            <h4 class="redtext">Lorem Ipsum</h4>
-                            <h3>Lorem Ipsum dolor sit amet, onsectetuer</h3>
-                            <p class="py-3 col-lg-6 col-sm-12">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis tenetur et autem explicabo pariatur itaque tempore voluptatum facere dolore veritatis, sapiente adipisci dolor deserunt sit quia quis deleniti eum ipsam!</p>
+                            <h4 class="redtext">Testimonials</h4>
+                            <h3>See what our clients have to say</h3>
+                            <p class="py-3 col-lg-6 col-sm-12">Don't just take our word for it, hear what our satisfied clients have to say about their experience working with us.</p>
                         </div>
                         <div class="col-lg-4 col-sm-12">
                             <div class="card">
                                 <img src="<?php echo get_template_directory_uri()?>/assets/img/icons/testimonial.png" class="img-thumbnail" alt="">
                                 <div class="card-body">
-                                    <h5>Your Topic</h5>
-                                    <p>Nulla luctus quam id ipsum tristique, sit amet mollis magna pellentesque. Nulla</p>
-                                    <p>Nulla luctus quam id ipsum tristique, sit amet mollis magna pellentesque. Nulla</p>
+                                    <h5>CFO THH Holdings</h5>
+									<h6>Kyle Fisher</h6>
+                                    <p>"Using The Jitu resources has allowed our team to better leverage our domestic talent and accomplish more in less time, a win, win." </p>
                                 </div>
                             </div>
                         </div>
+						
+						<div class="col-lg-4 col-sm-12">
+                            <div class="card">
+                                <img src="<?php echo get_template_directory_uri()?>/assets/img/icons/testimonial.png" class="img-thumbnail" alt="">
+                                <div class="card-body">
+                                    <h5>Sr. Consultant AcquireTek </h5>
+									<h6>Samir Vaghmar</h6>
+                                    <p>"Leveraging The Jitu resources has allowed our team to work more efficiently. We can achieve and deliver the end product much faster while maintaining the same scope of work with reduced costs." </p>
+                                </div>
+                            </div>
+                        </div>
+						
                         <div class="col-lg-4 col-sm-12">
                             <div class="card">
                                 <img src="<?php echo get_template_directory_uri()?>/assets/img/icons/testimonial.png" class="img-thumbnail" alt="">
                                 <div class="card-body">
-                                    <h5>Your Topic</h5>
-                                    <p>Nulla luctus quam id ipsum tristique, sit amet mollis magna pellentesque. Nulla</p>
-                                    <p>Nulla luctus quam id ipsum tristique, sit amet mollis magna pellentesque. Nulla</p>
+                                    <h5>4Cloud Services </h5>
+									<h6>Joshua Sarno</h6>
+                                    <p>"Partnering with The Jitu has allowed our company to expand service levels and decrease response time, enabling us to better focus and expand our client base. Great talent and commitment from everyone involved." </p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-sm-12">
-                            <div class="card">
-                                <img src="<?php echo get_template_directory_uri()?>/assets/img/icons/testimonial.png" class="img-thumbnail" alt="">
-                                <div class="card-body">
-                                    <h5>Your Topic</h5>
-                                    <p>Nulla luctus quam id ipsum tristique, sit amet mollis magna pellentesque. Nulla</p>
-                                    <p>Nulla luctus quam id ipsum tristique, sit amet mollis magna pellentesque. Nulla</p>
-                                </div>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
@@ -159,7 +162,9 @@ get_header();
             <div class="container-fluid mt-5 mb-5"> 
                 <div class="container text-center ">
                     <h3 class="text-uppercase">We are part of the THH-LLC <br> technology family</h3>
-                    <p>THE JITU brings dedicated remote software engineers from Kenya to the world, providing reliability and adaptability across business challenges to empowere IT teams.</p>
+                    <p>
+						THE Jitu is proud to work with a diverse group of partners and clients, ranging from small startups to multinational corporations. We believe in fostering strong, long-term relationships with all of our partners and clients, and strive to provide them with the highest level of service and support. Our commitment to excellence and innovation has earned us a reputation as a trusted and reliable partner in the industry.
+					</p>
                 </div>
             </div>
 
@@ -188,7 +193,7 @@ get_header();
                         <div class="col-lg-6 col-sm-12">
                             <h2 class="redtext text-uppercase">Let's Discuss Your<br/> Next Project</h2>
                             <p>
-                                Fill out the form or call us to set up a meeting. We’ll be happy to discuss your stang needs in detail, provide estimates, and answer all your questions.
+                                Fill out the form or call us to set up a meeting. We’ll be happy to discuss your staffing needs in detail, provide estimates, and answer all your questions.
                             </p>
                             <span class="">
                                 <i class="ri-phone-fill"></i> (+254) 710 453 693
