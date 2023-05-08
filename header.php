4b614503a52eba3d960ca2f9ec1a37c4480404d1
Maxwell Adwale <48594933+maxwelladwale@users.noreply.github.com>
2023-05-08 17:47:42 +0300

    fix header load issue

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title><?php echo the_title(); ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,9 +16,9 @@
     </head>
 
     <body <?php body_class(); ?>>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-jitu" id="d_topbar">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-jitu p-0" id="d_topbar">
             <div class="container menu-cont">
-                <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0 ul1">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ul1">
                     <li class="nav-item">
                         <a class="nav-link active link_p" aria-current="page" href="#">About</a>
                     </li>
@@ -28,25 +28,11 @@
                     <li class="nav-item">
                         <a class="nav-link link_p" href="#">Contacts</a>
                     </li>
-                </ul> -->
-
-                <?php
-                        wp_nav_menu( array(
-                            'theme_location'  => 'top-bar',
-                            'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-                            'container'       => 'ul',
-                            'container_class' => 'container menu-cont',
-                            'container_id'    => '',
-                            'menu_class'      => 'navbar-nav me-auto mb-2 mb-lg-0 ul1',
-                            'menu-item'		  => 'nav-item',
-                            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                            'walker'          => new WP_Bootstrap_Navwalker(),
-                        ) );
-                    ?>
+                </ul>
             </div> 
         </nav>
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white p-0">
             <div class="container">
                 <a class="navbar-brand" href="#">
                     <img src="<?php echo esc_url(get_template_directory_uri());?>/assets/img/logo-jitu-red.svg" alt="The Jitu Red Company logo" width="150" class="d-inline-block align-text-top">
@@ -62,26 +48,13 @@
                             <div style="padding-left: 20px;"><a class="nav-link link_p2" href="#">Contacts</a></div>
                         </span>
                     </div>
-
-                    <!-- <div class="navbar-nav ul2">
+                    <div class="navbar-nav ul2">
                         <a class="nav-link active link_p2" aria-current="page" href="#">Technology</a>
                         <a class="nav-link link_p2" href="#">BPO</a>
                         <a class="nav-link link_p2" href="#">Training</a>
-                    </div> -->
-
-                    <?php
-                        wp_nav_menu( array(
-                            'theme_location'  => 'top-menu',
-                            'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-                            'container'       => 'div',
-                            'container_class' => 'nav-lev1 collapse navbar-collapse',
-                            'container_id'    => 'navbarText',
-                            'menu_class'      => 'navbar-nav ul2',
-                            'menu-item'		  => 'nav-link',
-                            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                            'walker'          => new WP_Bootstrap_Navwalker(),
-                        ) );
-                    ?>
+                    </div>
                 </div>
             </div>
         </nav>
+
+        
