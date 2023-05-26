27b606d99489de9396b9a07a2ec420fc52a5c015
FreddieMwau <fredrickmutua12@gmail.com>
2023-05-26 15:41:06 +0300

    Complete UI design on PC view

--- template-parts/processes.php
+++ template-parts/processes.php
@@ -74,7 +74,7 @@ get_header();
 
                         <!-- Tabs -->
                         <div class="tbs d-flex align-items-start">
-                            <div class="nav flex-column  me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            <div class="nav flex-column me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                                 <p class="nav-tabs nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
                                     <i class="fa fa-plus-circle" aria-hidden="true"></i>
                                     Faster Turnaround Times
@@ -95,28 +95,71 @@ get_header();
                                     Cost savings
                                 </p>
                             </div>
-                            <div class="tab-content" id="v-pills-tabContent">
-                                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
-                                    <p>With teams working in different time zones, we can complete projects faster than companies with only one location. This means that our clients can get their projects completed faster, without sacrificing quality</p>
-                                    <button class="multi-country-text-button">VIEW OUR SERVICES</button>
-                                </div>
-                                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
-                                    <p>Our multi-country processes mean we provide our clients with more flexibility in project timelines and scheduling. We can work around our clients' schedules, ensuring their projects are completed on time and within budget.</p>
-                                    <button class="multi-country-text-button">VIEW OUR SERVICES</button>
-                                </div>
-                                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">
-                                    <p>With teams in both the United States and Kenya, we offer our clients the benefits of diverse perspectives and expertise. This means that our clients receive higher-quality services that meet their specific needs.</p>
-                                    <button class="multi-country-text-button">VIEW OUR SERVICES</button>
-                                </div>
-                                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
-                                    <p>By utilizing offshore teams, we offer our clients cost savings without sacrificing quality. This means that our clients receive high-quality web development services at a more affordable price.</p>
-                                    <button class="multi-country-text-button">VIEW OUR SERVICES</button>
+                            <div class="tbs-content">
+                                <div class="tab-content" id="v-pills-tabContent">
+                                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+                                        <p>With teams working in different time zones, we can complete projects faster than companies with only one location. This means that our clients can get their projects completed faster, without sacrificing quality</p>
+                                        <button class="multi-country-text-button">VIEW OUR SERVICES</button>
+                                    </div>
+                                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+                                        <p>Our multi-country processes mean we provide our clients with more flexibility in project timelines and scheduling. We can work around our clients' schedules, ensuring their projects are completed on time and within budget.</p>
+                                        <button class="multi-country-text-button">VIEW OUR SERVICES</button>
+                                    </div>
+                                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">
+                                        <p>With teams in both the United States and Kenya, we offer our clients the benefits of diverse perspectives and expertise. This means that our clients receive higher-quality services that meet their specific needs.</p>
+                                        <button class="multi-country-text-button">VIEW OUR SERVICES</button>
+                                    </div>
+                                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
+                                        <p>By utilizing offshore teams, we offer our clients cost savings without sacrificing quality. This means that our clients receive high-quality web development services at a more affordable price.</p>
+                                        <button class="multi-country-text-button">VIEW OUR SERVICES</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <!-- End Tabs -->
                     </div>
                 </div>
+
+                <!-- Start Accordions -->
+                <div class="accordion" id="accordionExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingOne">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        Accordion Item #1
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingTwo">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Accordion Item #2
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingThree">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        Accordion Item #3
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Accordions -->
             </div>
         </div>
     </div>
@@ -148,15 +191,17 @@ get_header();
 <section>
     <div class="container sdlcprocesses">
         <div class="row mt-5 mb-5">
-            <div class="col-md-6 col-sm-12 sdlcprocess">
-                <h1 class="redtext sdlc">Our SDLC process consists of the following stages:</h1>
-                <img src="<?php echo get_template_directory_uri() ?>/assets/img/icons/agile.png" class="img-thumbnail agile" alt="">
-                <!-- line -->
-                <span>
-                    <hr class="border_stage">
-                </span>
+            <div class="col-md-6 col-sm-12">
+                <div class="sdlcprocess">
+                    <h1 class="redtext sdlc">Our SDLC process consists of the following stages:</h1>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/icons/agile.png" class="img-thumbnail agile" alt="">
+                    <!-- line -->
+                    <span>
+                        <hr class="border_stage">
+                    </span>
 
-                <p class="closing_p">By following our comprehensive SDLC process, we ensure that our clients receive high-quality web development services that are delivered on time and within budget. Let us utilize our experience and help you achieve your business goals through effective software development.</p>
+                    <p class="closing_p">By following our comprehensive SDLC process, we ensure that our clients receive high-quality web development services that are delivered on time and within budget. Let us utilize our experience and help you achieve your business goals through effective software development.</p>
+                </div>
             </div>
 
             <div class="col-md-6 col-sm-12 stages">
